b086a921880019e2441144eb6766d4839a74d7f6
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-10-02 22:05:05 -0400

    revisiting this exercise

--- wordcount.py
+++ wordcount.py
@@ -1,47 +1,28 @@
+#task: import a string of words from a file and create a dictionary that keeps
+#track of word count.
+
 import sys
+from collections import Counter
 text_file = sys.argv[1]
 
 def make_word_count_dictionary(text_file):
-    words_to_count = open(text_file)
+    file = open(text_file).read()
+    words_list = file.split()
+    word_count_dict = {}
+    for word in words_list:
+        word = word.lower()
+        #strip end punctiation from a word
+        if not word[-1].isalpha():
+            end = word[-1]
+            word = word.strip(end)
+        word_count_dict[word] = word_count_dict.get(word, 0) + 1
 
-    word_count = {}
-    punctuation_tuple = ('.', ',', '!', '?', '"', ':', ';', '(', ')')
+    return word_count_dict
 
-    for line in words_to_count:
-        line = line.strip()
-        #print(line)
-        words = line.split(' ')
-        #print(words)
-        for word in words:
-            word = word.lower().strip()
-            # for letter in word:
-            #     if letter == '.' or letter == ',':
-            #         word.pop(letter)
-            if word.endswith(punctuation_tuple) or word.startswith(punctuation_tuple):
-                #print(word)
-                char_list = []
-                for char in word:
-                    if char not in punctuation_tuple:
-                        char_list.append(char)
-                #print(char_list)
-                word = ''.join(char_list)
-                # for char in word:
-                #     char.pop()
-            #     for index, letter in enumerate(word):
-            #         word.pop(-1)
-            #if word[-1] == '.':
-                #word.pop(-1)
-            word_count[word] = word_count.get(word, 0) + 1
+words = make_word_count_dictionary(text_file)
 
+def print_word_count(words):
+    for key, value in words.items():
+        print(key, value)
 
-        #adds each letter to dictionary
-        # for word in line:
-        #     word_count[word] = word_count.get(word, 0) + 1
-
-    for k, v in word_count.items():
-        print(k, v)
-
-make_word_count_dictionary(text_file)
-
-
-
+print_word_count(words)
